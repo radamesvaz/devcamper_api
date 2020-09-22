b20656132e99de445cd9f884801e500c5f28e0bf
Radames Vaz <rgvtovar@hotmail.com>
2020-09-22 11:04:58 -0400

    seeder para agregar todos los bootcamps

---
+++ seeder.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+//  Cargando las env vars
+dotenv.config({ path: './config/config.env' });
+
+//  Cargando el modelo que se va a subir a la base de datos
+const Bootcamp = require('./models/Bootcamp');
+
+//  Conectando a la base de datos
+ mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+
+//  Leyendo los archivos JSON que se van a subir
+const bootcamps = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+//  Importando archivos a la base de datos
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps);
+
+        console.log('Data importada...'.green.inverse);
+        process.exit();
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+//  Eliminando archivos de la base de datos
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+
+        console.log('Data eliminada...'.red.inverse);
+        process.exit();
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+if(process.argv[2] === '-i'){
+    importData();
+} else if(process.argv[2] === '-d'){
+    deleteData();
+}
